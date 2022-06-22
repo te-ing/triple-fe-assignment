05d9b0b456924a654a3d8715a86344f44bfa0381
te-ing <hi2177@naver.com>
2022-06-22 15:53:04 +0900

    feat: styled-components 및 reset 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 
 import App from '@/App'
 
+import GlobalStyles from './styles/globalStyles'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
 )
