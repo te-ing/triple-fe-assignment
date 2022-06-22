1e531e43deb769f3e09a59dd331a26b4b8c6f48f
te-ing <hi2177@naver.com>
2022-06-22 16:55:46 +0900

    fix: 타입지정을 통해 main.tsx warning 제거

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from '@/App'
 
 import GlobalStyles from './styles/globalStyles'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLInputElement).render(
   <React.StrictMode>
     <GlobalStyles />
     <App />
