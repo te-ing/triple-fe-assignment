dad8815f1ba1b5c7ce3bd0e2ef31604678c976c9
Taejung Kim <hi2177@naver.com>
2022-06-24 10:05:12 +0900

    feat: RiseAnimation delay 파라미터 타입 지정

--- src/components/Intro/styles.ts
+++ src/components/Intro/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 import { riseWithFadeIn } from '@/styles/commonKeyframes'
 
-export const RiseAnimation = (delay = 0) => styled.div`
+export const RiseAnimation = (delay?: number) => styled.div`
   opacity: 0;
-  animation: ease-in 0.7s ${riseWithFadeIn()} ${delay}s forwards;
+  animation: ease-in 0.7s ${riseWithFadeIn()} ${delay || 0}s forwards;
 `
